9adce67bd57fce462c1920d4655546e1dadb03cc
Antarip Saha <antarip15@gmail.com>
2023-03-01 17:34:06 +0600

    stable

--- proxyServer/urls.py
+++ proxyServer/urls.py
@@ -3,6 +3,7 @@ from .views import active_ips, ping_ips, available_ips, set_proxy
 
 urlpatterns = [
     path('active_ips/', active_ips, name='active_ips'),
+    path('', available_ips, name=''),
     path('ping_ips/', ping_ips, name='ping_ips'),
     path('available_ips/', available_ips, name='available_ips'),
     path('select_ip/<str:ip_port>/', set_proxy, name='select_ip')
--- proxyServer/views.py
+++ proxyServer/views.py
@@ -8,13 +8,14 @@ from django.shortcuts import render
 
 
 def active_ips(request):
-    url = 'https://proxylist.geonode.com/api/proxy-list?limit=500&page=1&sort_by=lastChecked&sort_type=desc'
+
+    url = 'https://proxylist.geonode.com/api/proxy-list?limit=30&page=1&sort_by=lastChecked&sort_type=desc'
     response = requests.get(url)
     data_str = response.content
     data_dict = json.loads(data_str)
     sorted_data = sorted(data_dict["data"], key=lambda x: x["latency"])
     ip_ports = [(d["ip"], d["port"]) for d in sorted_data]
-
+    print('active ips searching')
     # Write the IP addresses and ports to a text file
     with open("data.txt", "w") as f:
         for ip, port in ip_ports:
@@ -22,7 +23,21 @@ def active_ips(request):
 
     # Pass the list of IP addresses and ports to the template
     context = {"ip_ports": ip_ports}
-    return HttpResponse('All IPs Downloaded and saved')
+    # Open the data file and read all IP addresses and ports
+    with open("data.txt", "r") as f:
+        ip_ports = [line.strip() for line in f.readlines()]
+
+    # Ping each IP and write the ones without any lost packets to a new file
+    with open("available_ips.txt", "w") as f:
+        for ip_port in ip_ports:
+            ip, port = ip_port.split(":")
+            # Use the appropriate command for your OS to ping the IP address
+            # In this example, we're using the Windows ping command
+            response = os.system(f"ping -n 1 {ip}")
+            if response == 0:
+                f.write(f"{ip}:{port}\n")
+
+    return HttpResponse("Finished pinging IPs and wrote available IPs to file.")
     # return render(request, "available_ips.html", context)
 
 
@@ -79,6 +94,8 @@ def set_proxy(request, ip_port):
 
     # Check if the environment variables are set correctly
     if os.environ.get("http_proxy") == f"http://{ip_port}" and os.environ.get("https_proxy") == f"http://{ip_port}":
-        return HttpResponse(f"Proxy set to {ip_port}")
+        proxies = {"http": f"http://{ip_port}", "https": f"http://{ip_port}"}
+        response = requests.get('https://api.myip.com', proxies=proxies)
+        return HttpResponse(f"Your Current IP is: {response.text.strip()}")
     else:
         return HttpResponse("Error setting proxy")
